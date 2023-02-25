cfce8d1c23d2ff457032a436edaf8168905470ba
OctavianCorsei <octavian.corsei@gmail.com>
2023-02-25 19:05:00 +0000

    Add explanation comments to all the functions

--- javascript.js
+++ javascript.js
@@ -1,12 +1,17 @@
-//alert('ye');
 
+//variable initialisation
 let sizeOptionClick = document.getElementById('buttonCanvasSize');
 let canvasSize;
 let pixelSize;
 let canvas = document.getElementById('mainCanvas');
 let colorOptionPicker = document.getElementById('colorPicker');
 let currentColor = 'white';
+let mouseDown = 0;
 
+//add color palet (dinamically selected)
+addColorSet();
+
+//reset button
 let resetButton = document.getElementById('resetCanvas');
 resetButton.addEventListener('click', () => {
     var children = canvas.children;
@@ -16,12 +21,23 @@ resetButton.addEventListener('click', () => {
         }
 });
 
-addColorSet();
-
+//all event handlers
 sizeOptionClick.addEventListener('click', createCanvas);
-
+colorOptionPicker.addEventListener('click', pickColor);
 canvas.addEventListener('mouseover', fillPixel);
 
+//color the canvas while mouse pressed fucntionality
+canvas.addEventListener('mouseleave', () => {
+    mouseDown = 0;
+});
+canvas.onmousedown = function () {
+    mouseDown = 1;
+}
+canvas.onmouseup = function () {
+    mouseDown = 0;
+}
+
+//when selecting new canvas size replace the existing pixels
 function createCanvas(e){
     if(e.target.type == 'submit'){
         canvasSize = e.target.getAttribute('data-value');
@@ -32,28 +48,7 @@ function createCanvas(e){
     }
 }
 
-//check if mousedown or not
-let mouseDown = 0;
-
-canvas.addEventListener('mouseleave', () => {
-    mouseDown = 0;
-});
-
-canvas.onmousedown = function () {
-    mouseDown = 1;
-}
-canvas.onmouseup = function () {
-    mouseDown = 0;
-}
-
-function fillPixel(e){
-    if(mouseDown){
-        let thisPixel = e.target;
-        console.log(thisPixel);
-        thisPixel.style.backgroundColor = currentColor;
-    }
-}
-
+//add the correct number of pixels with the right classes on them
 function createPixels(canvasSize, pixelSize){
     for(let i = 0; i < canvasSize; i++){
         for(let j = 0; j < canvasSize; j++){
@@ -66,17 +61,35 @@ function createPixels(canvasSize, pixelSize){
 
 }
 
+//change the color of the brush to the new selected color
+function pickColor(e) {
+    if(e.target.classList.contains('color-option')){
+        currentColor = e.target.getAttribute('data-color');
+        console.log(currentColor);
+    }
+}
+
+//while mouse down fill the pixel that the mouse is on
+function fillPixel(e){
+    if(mouseDown){
+        let thisPixel = e.target;
+        console.log(thisPixel);
+        thisPixel.style.backgroundColor = currentColor;
+    }
+}
+
+//check that the canvas size is divisible by the no of pixerls
 function adjustCanvasSize(canvasSize){
     let wantedSize = canvas.clientHeight;
     while(wantedSize % canvasSize != 0){
         wantedSize -= 1;
         if(wantedSize < canvasSize)break;
     }
-    console.log(wantedSize);
     canvas.style.height = `${wantedSize}px`;
     canvas.style.width = `${wantedSize}px`;
 }
 
+//create new color and add it to the option list
 function createColorChoice(wantedColor){
     let newColor = document.createElement('div');
     newColor.classList.add('color-option');
@@ -85,6 +98,7 @@ function createColorChoice(wantedColor){
     colorOptionPicker.appendChild(newColor);
 }
 
+//color list create (at initial load)
 function addColorSet() {
     createColorChoice('#f4f4f4');
     createColorChoice('black');
@@ -100,13 +114,4 @@ function addColorSet() {
     createColorChoice('navy');
     createColorChoice('maroon');
     createColorChoice('olive');
-}
-
-colorOptionPicker.addEventListener('click', pickColor);
-
-function pickColor(e) {
-    if(e.target.classList.contains('color-option')){
-        currentColor = e.target.getAttribute('data-color');
-        console.log(currentColor);
-    }
 }
